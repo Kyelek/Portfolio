5a0fe90e240836ed8801eb370f4d3f22325d451d
Kyelek <fco.eugenio@hotmail.com>
2023-12-06 17:48:32 +0100

    arreglado extension imagenes

--- datos.js
+++ datos.js
@@ -3,49 +3,49 @@ const proyectos = [
         titulo:"Calculadora con Python",
         descripcion:"Uno de mis primeros proyectos, una calculadora creada en Python utilizando la libreria Tkinter",
         link:"https://github.com/Kyelek/CalculadoraPyTkinter",
-        imagen:"calculadoraPy.png",
+        imagen:"calculadoraPy.PNG",
         tecnologias:["Python | ", "Tkinter"]
     },
     {
         titulo:"Pokedex en Angular",
         descripcion:"Este proyecto consiste en una Pokédex hecha en Angular utilizando la PokeApi",
         link:"https://github.com/Kyelek/PokedexAngular",
-        imagen:"PokedexAngular.png",
+        imagen:"PokedexAngular.PNG",
         tecnologias:["Node.JS | ","Angular | ","HTML5 | ","SCSS | ","PokeApi"]
     },
     {
         titulo:"Calculadora para GTA Roleplay",
         descripcion:"Este programa surgió de la necesidad de hacer calculos rapidos de blanqueo para una mafia en GTARP",
         link:"https://github.com/Kyelek/calculadoraDineroGTARP",
-        imagen:"calculadoraGTARP.png",
+        imagen:"calculadoraGTARP.PNG",
         tecnologias:["Python"]
     },
     {
         titulo:"Reloj de Segmentos",
         descripcion:"El proyecto consiste en realizar un reloj a tiempo real utilizando segmentos",
         link:"https://github.com/Kyelek/RelojSegmentosJS",
-        imagen:"RelojSegmentos.png",
+        imagen:"RelojSegmentos.PNG",
         tecnologias:["JavaScript | ","HTML5 | ","CSS"]
     },
     {
         titulo:"Calculadora en JavaScript",
         descripcion:"Este proyecto no es la habitual calculadora con la funcion Eval(), tiene una vuelta de hoja muy interesante",
         link:"https://github.com/Kyelek/CalculadoraJS",
-        imagen:"CalculadoraJS.png",
+        imagen:"CalculadoraJS.PNG",
         tecnologias:["JavaScript | ","HTML5 | ","CSS"]
     },
     {
         titulo:"CRUD con Servlets en JAVA lanzado con TOMCAT",
         descripcion:"Este es un proyecto con el que aprendí a utilizar Tomcat y crear un CRUD en JAVA WEB utilizando Servlets en MAVEN",
         link:"https://github.com/Kyelek/Java_Crud_Tomcat",
-        imagen:"CrudTomcatMotos.png",
+        imagen:"CrudTomcatMotos.PNG",
         tecnologias:["JAVA | ","SQL | ","Tomcat | ","Servlets | ","MAVEN"]
     },
     {
         titulo:"Dungeon History",
         descripcion:"Un proyecto que empecé y no terminé, lo hice en un finde semana de vacaciones en un hotel y tiene implementadas cosas muy interesantes",
         link:"https://github.com/Kyelek/dungeon",
-        imagen:"DungeonGame.png",
+        imagen:"DungeonGame.PNG",
         tecnologias:["NodeJS | ","Angular | ","HTML5 | ", "CSS"]
     },
 
